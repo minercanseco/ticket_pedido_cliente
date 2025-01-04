a737ea38f6259dec74a155ee8805872522d1db4c
MinerCanseco <miner_canseco@hotmail.com>
2025-01-04 13:42:31 -0600

    commit inicial

---
+++ main.py
@@ -0,0 +1,5 @@
+from ticket_pedido_cliente import TicketPedidoCliente
+
+
+if __name__ == '__main__':
+    ticket = TicketPedidoCliente()
---
+++ ticket_pedido_cliente.py
@@ -0,0 +1,6 @@
+
+
+
+class TicketPedidoCliente:
+    def __init__(self):
+        pass
